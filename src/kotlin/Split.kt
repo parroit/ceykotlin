4b8ec0042f001d4b9cecedd005141ac8e42ad8e1
Andrea Parodi <parroit@ebansoft.com>
2012-07-21 23:00:45 +0200

    my2cents for Kotlin tuples

--- src/kotlin/Split.kt
+++ src/kotlin/Split.kt
@@ -1,5 +1,5 @@
-
-class Split(val string:String, val char:Char=':')/* : #{first:String,rest:String} */{
+//this is working
+class Split(val string:String, val char:Char=':')          {
     val index = string.indexOf(char)
     val first=string.get(0,index)
     val rest=string.get(index+1,string.size)
@@ -9,3 +9,20 @@ fun main(args:Array<String>){
     println(split.first)
     println(split.rest)
 }
+
+
+
+//This is what I would have preferred to write:
+/*
+class Split(val string:String, val char:Char=':'){
+    val index = string.indexOf(char)
+    val #{first,rest}=#{string.get(0,index),string.get(index+1,string.size)}
+}
+
+fun main(args:Array<String>){
+    val split = Split("hi:community");
+    println(split.first)
+    println(split.rest)
+}
+*/
+
